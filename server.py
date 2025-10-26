9592c70c0bcef4832c3fafdf7a5dd7e6ee5092ad
ecombassam <ecombassam@gmail.com>
2025-10-26 21:36:41 +0300

    Update server.py

--- server.py
+++ server.py
@@ -419,7 +419,7 @@ for x = 0 to array.size(sr_levels) - 1
         col = lvl < c240 ? color.new(color.lime, 0) : color.new(color.red, 0)
         array.set(sr_lines, x, line.new(bar_index - 1, lvl, bar_index, lvl, color=col, width=1, style=style, extend=extend.both))
 """
-// ===== labels for highest/lowest (from 240m series)
+   // ===== labels for highest/lowest (from 240m series)
 var label highestLabel = na
 var label lowestLabel  = na
 if drawhl
