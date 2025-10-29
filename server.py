c47451e6c8bdb61a6ca03f2ed85dc0120dfe4063
ecombassam <ecombassam@gmail.com>
2025-10-29 21:12:59 +0300

    Update server.py

--- server.py
+++ server.py
@@ -605,4 +605,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
